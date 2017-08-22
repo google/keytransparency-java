a97965fa23592492eb84886096ae999c8eef0ef6
AMarcedone <a.marcedone@gmail.com>
2017-08-22 15:25:42 -0700

    Update exampleapp

--- exampleapp/src/main/java/com/google/keytransparency/exampleapp/KeyTransparencyExample.java
+++ exampleapp/src/main/java/com/google/keytransparency/exampleapp/KeyTransparencyExample.java
@@ -5,13 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
-import com.google.keytransparency.exampleapp.R;
 import com.google.keytransparency.KTClient;
 import com.google.keytransparency.KeyTransparencyException;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class KeyTransparencyExample extends AppCompatActivity {
 
@@ -27,14 +24,16 @@ public class KeyTransparencyExample extends AppCompatActivity {
         try{
             tv.append("\n\n --- GetEntry test --- \n");
 
-            String ktUrl = "35.184.134.53:8080";
-            KTClient client = new KTClient(ktUrl, 1, getApplicationContext(), R.raw.server, R.raw.vrf_pubkey, R.raw.p256_pubkey, R.raw.trillian_log);
+            KTClient client = KTClient.getClient(1000);
             client.setTextViewForLogs(tv);
+            String ktUrl = "35.184.134.53:8080";
+            client.addKtServer(ktUrl, true, null, null);
+
             try {
                 String username = "gary.belvin@gmail.com";
 
                 tv.append("\nTrying to get public key for " + username + " from server " + ktUrl +"\n");
-                byte[] entry = client.getEntry(username,"app1",1000);
+                byte[] entry = client.getEntry(ktUrl, username,"app1");
                 if (entry==null){
                     tv.append("Received key is null: entry does not exists");
                 } else {
@@ -43,7 +42,7 @@ public class KeyTransparencyExample extends AppCompatActivity {
 
                 username = "NOT_A_USER@gmail.com";
                 tv.append("\n\nTrying to get public key for " + username + " from server " + ktUrl +"\n");
-                entry = client.getEntry(username,"app1",1000);
+                entry = client.getEntry(ktUrl, username,"app1");
                 if (entry==null){
                     tv.append("Received key is null: entry does not exists");
                 } else {
@@ -59,6 +58,10 @@ public class KeyTransparencyExample extends AppCompatActivity {
             e.printStackTrace();
             tv.append("\nError reading one of the public keys: "+e.getMessage());
             throw new RuntimeException("Error reading one of the public keys: "+e.getMessage(),e );
+        } catch (KeyTransparencyException e) {
+            e.printStackTrace();
+            tv.append("\nError creating the client: "+e.getMessage());
+            throw new RuntimeException("Error creating the client: "+e.getMessage(),e );
         }
     }
 
