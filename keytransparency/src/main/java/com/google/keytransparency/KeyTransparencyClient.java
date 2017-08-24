d6eb68cb915e35892989e45c8a1850c452ccd8e9
AMarcedone <a.marcedone@gmail.com>
2017-08-24 12:26:17 -0400

    Add static to all methods.

--- keytransparency/src/main/java/com/google/keytransparency/KeyTransparencyClient.java
+++ keytransparency/src/main/java/com/google/keytransparency/KeyTransparencyClient.java
@@ -6,13 +6,13 @@ public class KeyTransparencyClient {
 
     // addVerboseLogsDestination instructs the client to also send the verbose log statements
     // to the provided destination. Useful for demonstrating what the library is doing.
-    public void addVerboseLogsDestination(LogReceiver receiver) {
+    public static void addVerboseLogsDestination(LogReceiver receiver) {
         Gobindclient.addVerboseLogsDestination(receiver);
     }
 
     // setTimeout sets the timeout (in milliseconds) for all network requests made by the
     // KeyTransparencyClient.
-    public void setTimeout(int ms){
+    public static void setTimeout(int ms){
         Gobindclient.setTimeout(ms);
     }
 
@@ -20,7 +20,7 @@ public class KeyTransparencyClient {
     // the TLS certificate verification is skipped (useful for debug). If false, a self signed certificate
     // can be passed through ktTlsCertPem. Finally, domainInfoHash is the hash of all public key material
     // that will be used to verify the proofs provided by this kt server and should be hardcoded.
-    public void addKtServer(String ktUrl, boolean insecureTLS, byte[] ktTlsCertPem, byte[] domainInfoHash) throws KeyTransparencyException {
+    public static void addKtServer(String ktUrl, boolean insecureTLS, byte[] ktTlsCertPem, byte[] domainInfoHash) throws KeyTransparencyException {
         try {
             Gobindclient.addKtServer(ktUrl, insecureTLS, ktTlsCertPem, domainInfoHash);
         } catch (Exception e) {
@@ -30,7 +30,7 @@ public class KeyTransparencyClient {
 
     // getEntry retrieves an entry from a KeyTransparency server. Before calling this method, a connection
     // needs to be established through addKtServer.
-    public byte[] getEntry(String ktUrl, String userName, String appName) throws KeyTransparencyException {
+    public static byte[] getEntry(String ktUrl, String userName, String appName) throws KeyTransparencyException {
         try {
             return Gobindclient.getEntry(ktUrl, userName, appName);
         } catch (Exception e) {
